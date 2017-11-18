af5fadcf716c49cd55fa07bb94a19af200d4347e
thewisenerd <thewisenerd@protonmail.com>
2017-11-18 11:42:24 +0530

    tests: add cookies support to dummy server

--- tests/dummy-server.py
+++ tests/dummy-server.py
@@ -8,6 +8,7 @@
 
 from flask import Flask
 from flask import request
+from flask import make_response
 
 app = Flask(__name__)
 
@@ -19,6 +20,8 @@ from binascii import unhexlify
 import hashlib
 import time
 
+import collections
+
 def currentms():
 	return int(round(time.time() * 1000))
 
@@ -27,10 +30,9 @@ hashtable = (
 	'3644550032d201edab4bb729dc4b696af95f5d4b66365cdad26bb7ef9ab514a5',
 )
 
-(pubk, privk) = rsa.newkeys(1024, exponent=0x11)
-logintoken = hexlify(os.urandom(8)).decode('utf-8')
-gentime = currentms()
-(loginok, logintime) = (False, 0)
+Session = collections.namedtuple('Session', 'pubk privk logintoken gentime loginok logintime')
+
+lookuptable = {}
 
 def getsha256(s):
 	m = hashlib.sha256()
@@ -42,31 +44,13 @@ def revStrEncode(s):
 		s = "".join([s[i:i+2] for i in range(len(s)-2, -2, -2)])
 	return s
 
-@app.route('/GetStateAndView')
-def stateandview():
-	global loginok, logintime
-
-	ok = {
-		'view': 'Final'
-	}
-
-	auth = {
-		'view': 'Authentication'
-	}
-
-	if loginok and (currentms() - logintime < (250 * 1000)):
-		return json.dumps(ok)
-
-	return json.dumps(auth)
-
 @app.route('/RSASettings')
 def rsa_settings():
-	global pubk, privk, logintoken, gentime, loginok, logintime
 
-	(pubk, privk) = rsa.newkeys(1024, exponent=0x11)
+	session = hexlify(os.urandom(8)).decode('utf-8')
 	logintoken = hexlify(os.urandom(8)).decode('utf-8')
-	gentime = currentms()
-	loginok = False
+	(pubk, privk) = rsa.newkeys(1024, exponent=0x11)
+	lookuptable[session] = Session(pubk=pubk, privk=privk, logintoken=logintoken, gentime=currentms(), loginok=False, logintime=0)
 
 	payload = {
 		'm': '%0256x' % pubk.n,
@@ -74,13 +58,13 @@ def rsa_settings():
 		'loginToken': logintoken
 	}
 
-	return json.dumps(payload)
+	resp = make_response(json.dumps(payload))
+	resp.set_cookie('NACSID', session)
+
+	return resp
 
 @app.route('/Login', methods=['POST'])
 def login():
-	global pubk, privk, logintoken, gentime, hashtable, loginok, logintime
-
-	loginok = False
 
 	auth_failure = {
 		"context":"",
@@ -117,12 +101,18 @@ def login():
 		"delayInterval":"250"
 	}
 
+	if 'NACSID' not in request.cookies:
+		return json.dumps(failure)
+
+	if request.cookies['NACSID'] not in lookuptable:
+		return json.dumps(failure)
+
 	if 'realm' not in request.form:
-		return json.dumps(ret)
+		return json.dumps(failure)
 	if 'username' not in request.form:
-		return json.dumps(ret)
+		return json.dumps(failure)
 	if 'password' not in request.form:
-		return json.dumps(ret)
+		return json.dumps(failure)
 
 	if request.form['realm'] != 'passwordRealm':
 		return json.dumps(failure)
@@ -130,21 +120,47 @@ def login():
 	username = request.form['username']
 	password = request.form['password']
 
-	decrypted = rsa.decrypt(unhexlify(revStrEncode(password)), privk).decode('utf-8')
+	session = lookuptable[request.cookies['NACSID']]
+
+	decrypted = rsa.decrypt(unhexlify(revStrEncode(password)), session.privk).decode('utf-8')
 	token = decrypted[0:16]
 
-	if (token != logintoken):
+	if (token != session.logintoken):
 		return json.dumps(failure)
 
-	if (currentms() - gentime > (250 * 1000)):
+	if (currentms() - session.gentime > (250 * 1000)):
 		return json.dumps(session_failure)
 
 	if getsha256( username + ':' + decrypted[16:] ) in hashtable:
-		loginok = True
-		logintime = currentms()
+		lookuptable[request.cookies['NACSID']] = Session(pubk=session.pubk, privk=session.privk, logintoken=session.logintoken, gentime=session.gentime, loginok=True, logintime=currentms())
 		return json.dumps(success)
 
 	return json.dumps(auth_failure)
+
+@app.route('/GetStateAndView')
+def stateandview():
+
+	ok = {
+		'view': 'Final'
+	}
+
+	auth = {
+		'view': 'Authentication'
+	}
+
+	if 'NACSID' not in request.cookies:
+		return json.dumps(auth)
+
+	if request.cookies['NACSID'] not in lookuptable:
+		return json.dumps(auth)
+
+	session = lookuptable[request.cookies['NACSID']]
+
+	if session.loginok and (currentms() - session.logintime < (250 * 1000)):
+		return json.dumps(ok)
+
+	return json.dumps(auth)
+
 
 if __name__ == '__main__':
 	app.run()
